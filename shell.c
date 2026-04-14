751e23e903b819ba6b81d04f5b514c7ba59fc20d
maryem <zanned.maryem@gmail.com>
2026-04-14 13:14:18 +0100

    adding first modifications to shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,8 @@
 #include "shell.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
 /**
@@ -12,11 +14,20 @@
 int main(int argc, char *argv[])
 {
 	int status;
-	(void)argc;
+	char *prompt = "& ";
 
-	if (fork == 0)
+	if (argc < 2)
+	{
+		write(STDOUT_FILENO, "Usage: ./hsh command [args...]\n", 32);
+		return (EXIT_FAILURE);
+	}
+
+	write(STDOUT_FILENO, prompt, 2);
+	if (fork() == 0)
 	{
 		execvp(argv[1], argv + 1);
+		perror(argv[1]);
+		_exit(EXIT_FAILURE);
 	}
 	wait(&status);
 	return (EXIT_SUCCESS);
